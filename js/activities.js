4ce57a56d79cf2d30fbc93a618a505efea76cc10
Shifeng Hong <shifengh@uci.edu>
2025-10-16 23:03:53 -0700

    Part 2 completely implemented.

--- js/activities.js
+++ js/activities.js
@@ -9,77 +9,147 @@ function parseTweets(runkeeper_tweets) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
 
+	const activities = tweet_array
+		.filter(tweet => tweet.activityType !== 'unknown') // only convert for completed event
+		.map(tweet => ({ // map each to activity statistics
+			activityType: tweet.activityType,
+			distance: tweet.distance,
+			dayOfWeek: tweet.time.getDay()
+		}));
 
-	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
-
-	// remember to set to const after debugging
-	const activity_statistics = {}; // {activityName:string, count:number, maxDistance:number, minDistance:number}
-
-	for (const t of tweet_array){
-		const entry = activity_statistics[t.activityType];
-		if (entry === undefined) {
-			//create new entries
-			activity_statistics[t.activityType] = {count:1, maxDistance:t.distance, minDistance:t.distance};
-			continue;
-		}
-		else {
-			entry.count += 1;
-			entry.maxDistance = Math.max(entry.maxDistance, t.distance);
-			entry.minDistance = Math.min(entry.minDistance, t.distance);
-		}
-	}
-
-	// how many different activities got logged?
-	document.getElementById('numberActivities').innerText = Object.keys(activity_statistics).length;
-
-	// sort the entries by values, which is count of activities
-	const top_three_most_frequent = Array.from(Object.entries(activity_statistics)) // built array from key-value pairs, which turn into [key, value]
-									.sort(([a, a_stat], [b, b_stat]) => b_stat.count - a_stat.count) // b_count - a_count, larger value indicate b has more count than a
-									.slice(0,3);
-								
-	// which are the top three most frequent logged activities?
-	document.getElementById('firstMost').innerText = top_three_most_frequent[0][0];
-	document.getElementById('secondMost').innerText = top_three_most_frequent[1][0];
-	document.getElementById('thirdMost').innerText = top_three_most_frequent[2][0];
-
-	sort_by_distance = top_three_most_frequent.sort(([a, a_stat], [b, b_stat]) => b_stat.maxDistance - a_stat.maxDistance)
-
-	// what is the longest activity by distance?
-	document.getElementById('longestActivityType').innerText = sort_by_distance[0][0];
-
-	// what is the shortest activity by distance?
-	document.getElementById('shortestActivityType').innerText = sort_by_distance[2][0];
-
-	// weekday or weekend longer by distance?
-	tweet_array.forEach( t => {
-		let onSaturday = t.time.getDay() === 6; // let keyword make this only available in this block
-		let onSunday = t.time.getDay() === 0; 
-
-		if (onSaturday || onSunday) { 
-			totalDistance_weekend += t.distance;
-		}
-		else {
-			totalDistance_weekday += t.distance;
-		}
-	})
-
-	document.getElementById('weekdayOrWeekendLonger') = totalDistance_weekday
-
+	// plot #1: activity vs count
 	activity_vis_spec = {
 	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
 	  "description": "A graph of the number of Tweets containing each type of activity.",
 	  "data": {
-	    "values": tweet_array
+	    "values": activities
+	  },
+	  width: innerWidth * 0.8, // responsive to window size
+	  height: innerHeight * 0.6,
+	  mark: 'bar', // What type of graph?
+	  encoding: { //what to put on x and y axis?
+		x: {
+			field: 'activityType',
+			type: 'nominal', // categorical data
+			title: 'Activity' // xTitle
+		},
+		y: {
+			aggregate: 'count', // aggregate on the 'count' field
+			type: 'quantitative', //numerical data
+			title: 'Count' // yTitle
+		}
 	  }
-	  //TODO: Add mark and encoding
 	};
 	vegaEmbed('#activityVis', activity_vis_spec, {actions:false});
 
+	activityCount = {};
+	activities.forEach(t => {
+		activityCount[t.activityType] = (activityCount[t.activityType] || 0) + 1; // undefined || 0 will return 0, this is called short-circuit
+	});
+	
+	const top3_by_distance = Object.entries(activityCount)
+		.sort(([a, a_count], [b, b_count]) => b_count - a_count) //sort by higher count, notice this use the spread syntax
+		.slice(0, 3) // select first three
+		.map(x => x[0]) // preserve only the activityType
+
+	const top3_data = activities.filter(x => top3_by_distance.includes(x.activityType)); // select subset of activities which have one of the three activityType
+
+	const dayString = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+	// map index to String to clear visualization
+	top3_data.forEach( t => {
+		t.dayOfWeek = dayString[t.dayOfWeek]
+	})
+
 	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
 	//Use those visualizations to answer the questions about which activities tended to be longest and when.
+
+	// plot 2: 
+	distance_day_vis_spec = {
+	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+	  "description": "A plot of the distances by day of the week for all of the three most tweeted-about activities.",
+	  "data": {
+	    "values": top3_data
+	  },
+	  width: innerWidth * 0.8, // responsive to window size
+	  height: innerHeight * 0.6,
+	  mark: "point",
+	  encoding: {
+		x: {
+			field: 'dayOfWeek',
+			type: 'ordinal', //order by day
+			title: "time (day)",
+			sort: dayString
+		},
+		y:{
+			field: 'distance',
+			type: 'quantitative',
+			title: 'Distance'
+		},
+		color:{
+			field: 'activityType',
+			type: 'nominal',
+			title: 'Activity'
+
+		}
+	  }
+	};
+	vegaEmbed('#activityVis', distance_day_vis_spec, {actions:false});
+
+	const aggregate_btn = document.getElementById('aggregate');
+
+	let isAggregated = false; //default not aggregated
+
+	aggregate_btn.addEventListener('click', () => {
+		if (!isAggregated){
+			// change btn text
+			aggregate_btn.innerText = "Show All Activities";
+
+			// change encoding
+			distance_day_vis_spec.encoding.y = {
+				aggregate: 'mean',
+				field: 'distance',
+				type: 'quantitative',
+				title: 'Mean distance'
+			}
+		}
+		else {
+			isAggregated = true;
+			// change btn text
+			aggregate_btn.innerText = "Show Mean";
+
+			// change encoding
+			distance_day_vis_spec.encoding.y = {
+				field: 'distance',
+				type: 'quantitative',
+				title: 'Distance'
+			}
+		}
+
+		isAggregated = !isAggregated;
+		// finally, rerender
+		vegaEmbed('#activityVis', distance_day_vis_spec, {actions:false});
+	})
+
+
+
+	document.getElementById('numberActivities').innerText = Object.entries(activityCount).length;
+	// which are the top three most frequent logged activities?
+	document.getElementById('firstMost').innerText = 'run';
+	document.getElementById('secondMost').innerText = 'walk';
+	document.getElementById('thirdMost').innerText = 'bike';
+
+	// what is the longest activity by distance?
+	document.getElementById('longestActivityType').innerText = 'run'
+
+	// what is the shortest activity by distance?
+	document.getElementById('shortestActivityType').innerText = 'walk';
+
+	// weekday or weekend longer by average distance?
+	document.getElementById('weekdayOrWeekendLonger').innerText = "weekend";
 }
 
 //Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
 	loadSavedRunkeeperTweets().then(parseTweets);
-});
+}); 
